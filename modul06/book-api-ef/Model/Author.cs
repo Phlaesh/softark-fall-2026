0c2fa53fdcec0a6f89c4b37a0374f3bd942953a7
Phlaesh <114645175+Phlaesh@users.noreply.github.com>
2026-09-14 11:39:12 +0200

    Update Author.cs

--- modul06/book-api-ef/Model/Author.cs
+++ modul06/book-api-ef/Model/Author.cs
@@ -4,6 +4,6 @@ namespace Model
     {
         public int AuthorId { get; set; }
         public string Fullname { get; set; }
-        public List<Book> Books { get; set; } //= new List<Book>(); // fix i git, problem hvis direkte anvendt
+        public List<Book> Books { get; set; } = new List<Book>(); 
     }
 }
